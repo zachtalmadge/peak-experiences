35ed7e684e10089acc2339d7bcf35a4fd8ef0c38
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-10-17 15:06:16 -0700

    add Link to button

--- src/Components/BucketListCard/BucketListCard.js
+++ src/Components/BucketListCard/BucketListCard.js
@@ -1,18 +1,17 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
 
-function BucketListCard({ image, name, location, risk, isInList }) {
-  isInList = true;
+function BucketListCard({ image, name, location, risk, isInList, id }) {
   return (
     <Card>
       <Card.Img variant="top" src={image} />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
         <Card.Text>{location}</Card.Text>
-        <Button variant="primary">Details</Button>
-        <Button variant="primary" disabled={isInList ? true : false}>
-          Add to my list
-        </Button>
+        <Link to={`/activites/${id}`}>
+          <Button variant="primary">Details</Button>
+        </Link>
       </Card.Body>
     </Card>
   );
--- src/routes.js
+++ src/routes.js
@@ -1,37 +1,37 @@
-import App from './App'
-import Home from './Pages/Home'
-import AllActivites from './Pages/AllActivites'
-import ActivityDetails from './Pages/ActivityDetails'
-import UserList from './Pages/UserList'
-import Form from './Pages/Form'
+import App from "./App";
+import Home from "./Pages/Home";
+import AllActivites from "./Pages/AllActivites";
+import ActivityDetails from "./Pages/ActivityDetails";
+import UserList from "./Pages/UserList";
+import Form from "./Pages/Form";
 
 const routes = [
-    {
-        path: '/',
-        element: <App />,
-        children: [
-            {
-                path: '/',
-                element: <Home />
-            },
-            {
-                path: '/activites',
-                element: <AllActivites />
-            },
-            {
-                path: '/activites/:id',
-                element: <ActivityDetails />
-            },
-            {
-                path: '/mybucketlist',
-                element: <UserList />
-            },
-            {
-                path: '/form',
-                element: <Form />
-            }
-        ]
-    }
-]
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/activites",
+        element: <AllActivites />,
+      },
+      {
+        path: "/activites/:id",
+        element: <ActivityDetails />,
+      },
+      {
+        path: "/mybucketlist",
+        element: <UserList />,
+      },
+      {
+        path: "/form",
+        element: <Form />,
+      },
+    ],
+  },
+];
 
-export default routes
+export default routes;
