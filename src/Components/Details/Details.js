91919b40a725b72f389fdc50c1328418d4e879fa
Landon <landoncramer1@gmail.com>
2023-10-17 16:19:52 -0700

    add Details component

---
+++ src/Components/Details/Details.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Details({name, image, description, category, risk, id}) {
+    
+console.log("this is coming from the details component:",name, image, description, category, risk, id)
+  return (
+    <div>Details</div>
+  )
+}
+
+export default Details
