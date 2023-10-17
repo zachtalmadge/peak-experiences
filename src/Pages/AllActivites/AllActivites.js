e9a9c7aab994a1a93eb555d2539ffc1c85d05d36
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-10-17 14:56:48 -0700

    render bucketlist card

--- src/Pages/AllActivites/AllActivites.js
+++ src/Pages/AllActivites/AllActivites.js
@@ -1,26 +1,28 @@
-import { useState } from 'react'
-import { useOutletContext } from "react-router"
-import FilterByCategory from '../../Components/FilterByCategory'
+import { useState } from "react";
+import { useOutletContext } from "react-router";
+import FilterByCategory from "../../Components/FilterByCategory";
+import BucketListCard from "../../Components/BucketListCard";
 
 const AllActivites = () => {
+  let [activites] = useOutletContext();
+  let [category, setCategory] = useState("");
 
-    let [ activites ] = useOutletContext()
-    let [ category, setCategory ] = useState('')
+  const changeCategory = (e) => {
+    setCategory(e.target.value);
+  };
 
-    const changeCategory = (e) => {
-        setCategory(e.target.value)
-    }
+  let filteredActivites = activites.filter((activity) => {
+    return category ? activity.category.toLowerCase() === category : activity;
+  });
 
-    let filteredActivites = activites.filter(activity => {
-        return category ? activity.category.toLowerCase() === category : activity
-    })
+  return (
+    <>
+      <FilterByCategory changeCategory={changeCategory} />
+      {filteredActivites.map((activity) => (
+        <BucketListCard {...activity} key={activity.id} />
+      ))}
+    </>
+  );
+};
 
-    return (
-        <>
-        <FilterByCategory changeCategory={changeCategory} />
-        <h1>Hello! I am the All Activities Page!</h1>
-        </>
-    )
-}
-
-export default AllActivites
+export default AllActivites;
