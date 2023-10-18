c9e588d18070874f5d51f0dedb620f3bcd003565
Landon <landoncramer1@gmail.com>
2023-10-18 12:51:30 -0700

    Details page

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,23 +1,25 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-function Details({name, image, description, category, risk, id}) {
-    
+function Details({ name, image, description, category, risk, id, location }) {
   return (
-    <div>
-    <img src={image} alt={name} />
-    <div>
-      <h2>{name}</h2>
-      <p>{category}</p>
-      <p>{risk}</p>
-      <p>{description}</p>
-    </div>
-    <Link to={'/activites'}>
-    <button data-activity-id={id}>
-     All Activities
-    </button>
-    </Link>
-  </div>
-  )
+    <Container>
+      <Row>
+        <Col>
+          <img src={image} className="img-fluid" alt={name} />
+        </Col>
+        <Col>
+          <h1>{name}</h1>
+          <p>{description}</p>
+          <p>{category}</p>
+          <p>{risk}</p>
+          <p>{location}</p>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 export default Details;
