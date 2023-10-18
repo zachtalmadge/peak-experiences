9d9d528d11053546681791be1fc53a23dd979d30
Landon <landoncramer1@gmail.com>
2023-10-17 18:05:35 -0700

    displaying data on details pg

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -4,7 +4,18 @@ function Details({name, image, description, category, risk, id}) {
     
 console.log("this is coming from the details component:",name, image, description, category, risk, id)
   return (
-    <div>Details</div>
+    <div>
+    <img src={image} alt={name} />
+    <div>
+      <h2>{name}</h2>
+      <p>{category}</p>
+      <p>{risk}</p>
+      <p>{description}</p>
+    </div>
+    <button data-activity-id={id}>
+      Add to Bucket List
+    </button>
+  </div>
   )
 }
 
