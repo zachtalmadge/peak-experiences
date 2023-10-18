518723284942ae7b39c1c739e533117a8a6bf098
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-10-18 18:27:35 -0400

    display two random activities features working

--- src/Components/FeaturedActivites/FeaturedActivites.js
+++ src/Components/FeaturedActivites/FeaturedActivites.js
@@ -1,20 +1,46 @@
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import BucketListCard from '../BucketListCard'
+import { useFetch } from '../../Hooks/customHooks'
+let URL1 = "http://localhost:3000/bucketListItems";
+
 
 const FeaturedActivites = () => {
+
+    let { data } = useFetch(URL1)
+
+    function pickTwoDistinctRandomElements(arr) {
+        if (arr.length < 2) {
+            console.log('Not enough elements in the array to pick two distinct items.');
+            return null;
+        }
+    
+        const index1 = Math.floor(Math.random() * arr.length);
+        let index2;
+    
+        do {
+            index2 = Math.floor(Math.random() * arr.length);
+        } while (index1 === index2);
+    
+        return [arr[index1], arr[index2]];
+    }
+
+    let featured = pickTwoDistinctRandomElements(data)
+    
 
     return (
         <Container className="my-5">
             <h1 className="text-center">Featured Activites</h1>
             <Row>
                 <Col>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt totam est vel. Facere officiis nihil, consequatur repudiandae earum odit sit minus maiores? Earum libero at corporis in labore, voluptas a!Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt totam est vel. Facere officiis nihil, consequatur repudiandae earum odit sit minus maiores? Earum libero at corporis in labore, voluptas a!Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt totam est vel. Facere officiis nihil, consequatur repudiandae earum odit sit minus maiores? Earum libero at corporis in labore, voluptas a!</p>
+                    {featured ? <BucketListCard {...featured[0]} /> : '' }
                 </Col>
             </Row>
             <Row>
                 <Col>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt totam est vel. Facere officiis nihil, consequatur repudiandae earum odit sit minus maiores? Earum libero at corporis in labore, voluptas a!Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt totam est vel. Facere officiis nihil, consequatur repudiandae earum odit sit minus maiores? Earum libero at corporis in labore, voluptas a!Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt totam est vel. Facere officiis nihil, consequatur repudiandae earum odit sit minus maiores? Earum libero at corporis in labore, voluptas a!</p>
+                {featured ? <BucketListCard {...featured[1]} /> : '' }
+
                 </Col>
             </Row>
         </Container>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,15 @@
 import Jumbotron from '../../Components/Jumbotron'
 import IntroBlurb from '../../Components/IntroBlurb'
 import FeaturedActivites from '../../Components/FeaturedActivites'
+import {useOutletContext} from 'react-router'
 
 const Home = () => {
+
     return (
         <>
         <Jumbotron />
         <IntroBlurb />
-        <FeaturedActivites />
+        <FeaturedActivites/>
         </>
     )
 }
